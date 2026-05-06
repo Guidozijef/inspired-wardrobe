60973871051faf0fb2943e901564e5b7936eec0a
张剑锋 <913497146@qq.com>
2026-05-06 23:23:45 +0800

    获取地址经纬度坐标系修改

--- pages/home/home.js
+++ pages/home/home.js
@@ -64,7 +64,7 @@ Page({
   fetchWeather() {
     // 优先尝试获取手机 GPS 定位 (使用模糊定位，更容易通过审核)
     wx.getLocation({
-      type: 'wgs84',
+      type: 'gcj02',
       success: (res) => {
         this.callWeatherCloudFunction(res.latitude, res.longitude);
       },
@@ -81,8 +81,10 @@ Page({
       name: 'clothFunctions',
       data: { 
         type: 'getWeather',
-        lat: lat,
-        lon: lon
+        data: {
+          lat: lat,
+          lon: lon
+        }
       }
     }).then(res => {
       console.log(res)
